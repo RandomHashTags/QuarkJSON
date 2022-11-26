abfce615f103429d9983c74e82ded6bf635287b0
RandomHashTags <ImRandomHashTags@gmail.com>
2022-11-26 03:02:23 -0600

    Initial Commit

---
+++ QuarkJSON/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  QuarkJSON
+//
+//  Created by Evan Anderson on 11/26/22.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
